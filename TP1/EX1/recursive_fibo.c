507198579ddec2196fe0860c68bdbc45ea0b47a1
LEFDAOUI-Wissal <72799001+LEFDAOUI-Wissal@users.noreply.github.com>
2021-03-14 21:48:47 +0100

    Update recursive_fibo.c

--- TP1/EX1/recursive_fibo.c
+++ TP1/EX1/recursive_fibo.c
@@ -1,6 +1,13 @@
 #include "header.h"
+#include <stdio.h>
 
-int recursive_fibo(int n)
+int recursive_fibo(unsigned n)
 {
-    // write your recursive fibo code here
+   if (n==0)
+		return 0;
+	if (n==1)
+		return 1;
+	else	
+		int m=recursive_fibo(n-1)+recursive_fibo(n-2);
+	return m; 
 }
